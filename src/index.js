5bde942a086621609cba03eb23e941e7a61a4b8f
Samet <sametselki@outlook.com>
2024-04-29 15:43:45 +0300

    syntax edit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App";
 import BookContextProvider from "./context/BookContext";
 import ThemeContextProvider from "./context/ThemeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeContextProvider>
